6cc3c1f45078bb9a1f831293376af29dacf09312
Doreen Tiema <doreentiema87@gmail.com>
2024-03-13 06:55:10 +0300

    creating conn.php file for the connection to the database

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+
+    define("HOST", "localhost");
+    define("USERNAME","root");
+    define("PASSWORD","");
+    define("DATABASE","school");
+
+    $conn = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+
+?>
