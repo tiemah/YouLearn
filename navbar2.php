dd56afae1ec4187a3b6bc9857717f997250e03e2
Doreen Tiema <doreentiema87@gmail.com>
2024-03-19 06:06:22 +0300

    linking of paages

--- navbar2.php
+++ navbar2.php
@@ -11,8 +11,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="./index.php#about" class="nav-item nav-link">About</a>
                 <a href="courses.html" class="nav-item nav-link">Courses</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
