a08b0b58375ad4d89b1317938fa0e70f67dfcd9b
Doreen Tiema <doreentiema87@gmail.com>
2024-03-28 15:02:49 +0300

    linking of pages in the navbar

--- navbar.php
+++ navbar.php
@@ -11,17 +11,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="courses.html" class="nav-item nav-link">Courses</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu fade-down m-0">
-                        <a href="team.html" class="dropdown-item">Our Team</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        <a href="404.html" class="dropdown-item">404 Page</a>
-                    </div>
-                </div>
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="index.php#about" class="nav-item nav-link">About</a>
+                <a href="index.php#courses" class="nav-item nav-link">Courses</a>
+                
                 <a href="contact.html" class="nav-item nav-link">Contact</a>
             </div>
             <a href="login.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
