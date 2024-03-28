cb4022511243b54b9135e5337ee965816ffec17b
Doreen Tiema <doreentiema87@gmail.com>
2024-03-28 12:28:55 +0300

    adding buttons and making the page responsive

--- add-course.php
+++ add-course.php
@@ -10,11 +10,11 @@
         require_once "navbar2.php";
         require_once "styles.php";
     ?>
-    <div class="row">
+    <div class="row mb-5">
         <div class="col-lg-4"></div>
 
         <div class="col-lg-4">
-            <h2 class="mt-4">Add Course</h2>
+            <h2 class="mt-4 text-center">Add Course</h2>
             <form action="" method="POST">
                 
                 <div class="mt-1">
@@ -25,8 +25,10 @@
                 <label for="exampleFormControlTextarea1" class="form-label text-dark">Course Description</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" name="course-description" placeholder="e.g Laws of Software" rows="3"></textarea>
                 </div>
-
-                <button class="btn btn-primary" name="course-btn" value="submit">Add</button>
+                 <div class="buttons mx-5 mt-3">
+                <button class="btn btn-primary" name="course-btn" value="submit" style="border-radius: 20px;">Add</button>
+                <button class="btn btn-primary" value="reset" style="border-radius: 20px; margin-left:130px;">Reset</button>
+                 </div>
             </form>
         </div>
     </div>
@@ -34,5 +36,10 @@
     <?php
         require_once "footer.php";
     ?>
+    <!-- JavaScript Libraries -->
+ <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    
 </body>
 </html>
