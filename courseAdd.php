e0118a7638b866e7d54dc6756fdf7accc75a2f8f
Doreen Tiema <doreentiema87@gmail.com>
2024-03-28 12:52:44 +0300

    ensuring the swal message executes after DOM has loaded

--- courseAdd.php
+++ courseAdd.php
@@ -5,23 +5,21 @@
         $course_description = mysqli_real_escape_string($conn, $_POST['course_description']);    
 
         // inserting into the database
-        $query = mysqli_query($conn, "INSERT INTO courses(course_name, course_description)VALUES('$course_name', '$course_description')") or die(mysqli_error($conn));
+        $query = mysqli_query($conn, "INSERT INTO courses(course_name, course_description) VALUES ('$course_name', '$course_description')") or die(mysqli_error($conn));
         // swal success message
         if($query){
-            // header("Location: login.php");
-                // exit();
-            ?>
+?>
             <!-- sweetalert link -->
             <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
             
             <script>
-                swal("Good job!", "Course added successfully!", "success");
-                </script>
-           
-                <?php
-                
-                
+                document.addEventListener('DOMContentLoaded', function() {
+                    swal("Good job!", "Course added successfully!", "success").then(() => {
+                        window.location.href = "add-course.php"; // Redirect to add-course page after displaying sweet alert
+                    });
+                });
+            </script>
+<?php
+        }
     }
-    }
-
 ?>
