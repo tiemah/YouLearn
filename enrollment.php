c4bc6174fe2adb575f1ecaeb1f02d8b79380c071
Doreen Tiema <doreentiema87@gmail.com>
2024-03-31 11:19:07 +0300

    enrollment page picks data from the database successfully

--- enrollment.php
+++ enrollment.php
@@ -9,13 +9,19 @@
 </head>
 <body>
     <?php
-      require_once "navbar2.php";
-      require_once "styles.php";
+        require_once "navbar2.php";
+        require_once "styles.php";
+        require_once "conn.php"; // Include the file containing database connection details
+
+        // Query to fetch course data from the database
+        $query = "SELECT * FROM courses";
+        $result = mysqli_query($conn, $query);
     ?>
     
-    <div class="row mt-5">
+    <div class="row mt-0">
         <div class="col-lg-2 bg-primary mt-5">
-        <ul class="mt-3">
+            <!-- Sidebar content -->
+            <ul class="mt-3">
                 <li style="list-style: none;" class="text-light mx-3 mt-4"><i class="bi bi-person"></i>&nbsp;<a href="profile.php" class="text-light">Profile</a></li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-check-square"></i>&nbsp;<a href="enrollment.php" class="text-light">Course enrollment</a></li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-book"></i>&nbsp;View Courses</li><br>
@@ -26,91 +32,43 @@
         <div class="col-lg-10 mt-5">
             <table class="table table-hover">
                 <thead class="table-dark">
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Course Title</th>
-                    <th scope="col">Course Code</th>
-                    <th scope="col">Semester</th>
-                    <th scope="col" colspan="2" class="text-center">Action</th>
-                    <!-- <th scope="col">Action</th> -->
-                  </tr>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Course Title</th>
+                        <th scope="col">Course Description</th>
+                        <th scope="col">Enroll</th>
+                        <th scope="col">View</th>
+                        <!-- <th scope="col" colspan="2" class="text-center">Action</th> -->
+                    </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Engineering and Software Law</td>
-                    <td>COM 421</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">6</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">7</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">8</th>
-                    <td>Neural Networks</td>
-                    <td>COM 424</td>
-                    <td>Y4S2</td>
-                    <td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>
-                    <td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>
-                  </tr>
+                    <?php
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        $count = 1;
+                        // Loop through the fetched course data
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            // Output table rows for each course
+                            echo '<tr>';
+                            echo '<th scope="row">' . $count++ . '</th>';
+                            echo '<td>' . $row['course_name'] . '</td>';
+                            echo '<td>' . $row['course_description'] . '</td>';
+                            echo '<td><button type="submit" class="btn btn-success" name="enroll" style="border-radius: 20px;">Enroll</button></td>';
+                            echo '<td><button type="submit" class="btn btn-danger" name="discard" style="border-radius: 20px;">Discard</button></td>';
+                            echo '</tr>';
+                        }
+                    } else {
+                        echo '<tr><td colspan="4">No courses found.</td></tr>'; // Displayed if no courses are available in the database
+                    }
+                    ?>
                 </tbody>
-              </table>
+            </table>
         </div>
     </div>
     <?php
-       require_once "footer.php";
+        require_once "footer.php";
     ?>
-    
 
     <!-- bootstrap js cdn link -->
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
