1010a2eb384ff9b40f1ed57d345a635af49b4ad1
Doreen Tiema <doreentiema87@gmail.com>
2024-04-02 16:53:50 +0300

    admin-dashboard page

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -1,15 +1,18 @@
 <?php
 // Start session if not already started
 session_start();
-
+$email = $_SESSION['login']; // Get the email of logged in user from session
 // Check if user is logged in and has admin role
-if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'administrator') {
+// if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'administrator') {
     // Redirect user to another page or display an access denied message
-    header("Location: login.php");
-    exit(); // Stop further execution
-}
+    // header("Location: login.php");
+    // exit(); // Stop further execution
+// }
 
 // Continue rendering the page content for admins
+require_once "navbar2.php";
+require_once "styles.php";
+require_once "conn.php";
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +21,69 @@ if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'administrator'
     <title>Admin Page</title>
 </head>
 <body>
-    <h1>Welcome, Admin!</h1>
-    <!-- Admin-only content here -->
+    <h1 class="text-center">Welcome, Admin!</h1>
+    <div class="row">
+    <div class="col-lg-2 bg-primary">
+        <ul class="mt-3">
+                <li style="list-style: none;" class="text-light mx-3 mt-3"><i class="bi bi-house"></i>&nbsp;<a href="admin-dashboard.php" class="text-light">Home</li><br>
+                <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-person"></i>&nbsp;<a href="profile.php" class="text-light">Profile</a></li><br>
+                <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-check-square"></i>&nbsp;<a href="users.php" class="text-light">Manage users</a></li><br>
+                <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-book"></i>&nbsp;<a href="courses.php" class="text-light">Manage courses</a></li><br>
+                <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-box-arrow-right"></i>&nbsp;<a href="logout.php" class="text-light">Logout</a></li><br>
+                
+            </ul>
+        </div>
+        <div class="col-lg-10">
+        <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+                            <h5 class="mb-3">Software Law</h5>
+                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-globe text-primary mb-4"></i>
+                            <h5 class="mb-3">HCI design</h5>
+                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-home text-primary mb-4"></i>
+                            <h5 class="mb-3">Neural Networks</h5>
+                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
+                            <h5 class="mb-3">Modelling </h5>
+                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+    </div>
+    <?php
+        require_once "footer.php";
+    ?>
 </body>
 </html>
