fee024d9e016cf67758f77564f86e74b7d2dcc44
Doreen Tiema <doreentiema87@gmail.com>
2024-04-06 22:10:57 +0300

    footer

--- student-dashboard.php
+++ student-dashboard.php
@@ -1,5 +1,31 @@
+<?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit(); // Stop further execution
+}
+
+// Include database connection
+require_once "conn.php";
+
+// Retrieve user's first name from the database
+$email = $_SESSION['login'];
+$query = "SELECT firstname FROM students WHERE email = '$email'";
+$result = mysqli_query($conn, $query);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $firstName = $row['firstname'];
+} else {
+    $firstName = ''; // Set default value if first name not found
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,145 +33,38 @@
     <!-- link to bootstrap css cdn link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
+
     <?php
-      require_once "navbar2.php";
-      require_once "styles.php";
-      // Check if user is logged in and has admin role
-    if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'registered_user') {
-    // Redirect user to login page 
-    // header("Location: login.php");
-    // exit(); // Stop further execution
-}
-
-      session_start();
-    
-if(!isset($_SESSION['login'])){
-    header("Location:login.php");
-}
-
+    require_once "navbar2.php";
+    require_once "styles.php";
     ?>
-    
-    <div class="row mt-3">
+
+    <div class="row mt-0">
         <div class="col-lg-2 bg-primary">
-        <ul class="mt-3">
+            <ul class="mt-3">
                 <li style="list-style: none;" class="text-light mx-3 mt-3"><i class="bi bi-house"></i>&nbsp;<a href="student-dashboard.php" class="text-light">Dashboard</li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-person"></i>&nbsp;<a href="profile.php" class="text-light">Profile</a></li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-check-square"></i>&nbsp;<a href="enrollment.php" class="text-light">Course enrollment</a></li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-book"></i>&nbsp;<a href="view-courses.php" class="text-light">View Courses</a></li><br>
                 <li style="list-style: none;" class="text-light mx-3"><i class="bi bi-box-arrow-right"></i>&nbsp;<a href="logout.php" class="text-light">Logout</a></li><br>
-                
+
             </ul>
         </div>
         <div class="col-lg-10">
-            <!-- <h2 class="text-center mt-3">Offered Courses</h2> -->
-            <!--  display the course information here -->
-            <!-- Service Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-4">
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-                            <h5 class="mb-3">Software Law</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-globe text-primary mb-4"></i>
-                            <h5 class="mb-3">HCI design</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-home text-primary mb-4"></i>
-                            <h5 class="mb-3">Neural Networks</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
-                            <h5 class="mb-3">Modelling </h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <!-- Display welcome message with user's first name -->
+            <h2 class="mt-3">Welcome, <?php echo $firstName; ?>!</h2>
+            <!-- Rest of your HTML content -->
         </div>
     </div>
-    <!-- Service End -->
-    <!-- Service Start -->
-    <!-- <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-4">
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-                            <h5 class="mb-3">Simulation</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-globe text-primary mb-4"></i>
-                            <h5 class="mb-3">OOP</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-home text-primary mb-4"></i>
-                            <h5 class="mb-3">Data Structures</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="service-item text-center pt-3">
-                        <div class="p-4">
-                            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
-                            <h5 class="mb-3">Basic Calculus</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-                            <button class="btn btn-success" style="border-radius: 20px;" name="course">Enroll</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- Service End -->
-    
-        </div>
-    </div>
+
     <?php
-       require_once "footer.php";
+    require_once "footer.php";
     ?>
-    
 
     <!-- bootstrap js cdn link -->
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
